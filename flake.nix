9a119a07c575caae2bacce3fe7cc75ca836c53fa
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-03-11 09:58:39 +0100

    Add charts env
    Signed-off-by: William Phetsinorath <william.phetsinorath@shikanime.studio>
    Change-Id: I4971cc11369fd12bef8e63c8ccbb47f96a6a6964
    ghstack-source-id: b7eb534c41f03a8b57972713572d4590d0c452e5
    Pull-Request: https://github.com/shikanime/minishells/pull/31

--- flake.nix
+++ flake.nix
@@ -130,6 +130,14 @@
               };
             };
 
+            "cloud-pi-native/helm-charts" = {
+              imports = [ cloud-pi-native ];
+
+              languages.helm.enable = true;
+
+              packages = with pkgs; [ chart-testing ];
+            };
+
             "cloud-pi-native/socle" = {
               imports = [ cloud-pi-native ];
 
